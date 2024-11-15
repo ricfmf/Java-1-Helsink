1ec83615cd534b9678773175848d98b2828471bc
ricfmf <75329136+ricfmf@users.noreply.github.com>
2024-11-14 21:54:43 -0300

    Add files via upload
    Curso Java Helsink parte 1 commit 3

---
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class DifferentTypesOfInput {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give a string: ");
+        String text = scan.nextLine();
+        System.out.println("Give an integer: ");
+        int num1 = Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double: ");
+        double num2 = Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean: ");
+        boolean value = Boolean.valueOf(scan.nextLine());
+        System.out.println("You gave the string " + text);
+        System.out.println("You gave the integer " + num1);
+        System.out.println("You gave the double " + num2);
+        System.out.println("You gave the boolean " + value);
+    }
+}
