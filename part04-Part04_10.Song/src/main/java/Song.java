078e34f308b1afdb9e51055a058cc077fc541842
ricfmf <75329136+ricfmf@users.noreply.github.com>
2024-11-14 22:23:40 -0300

    Add files via upload

---
+++ part04-Part04_10.Song/src/main/java/Song.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ricar
+ */
+public class Song {
+    private String name;
+    private int lengthInSeconds;
+    
+    public Song(String name, int length) {
+        this.name = name;
+        this.lengthInSeconds = length;
+    }
+    
+    public String name() {
+        return this.name;
+    }
+    
+    public int length() {
+        return this.lengthInSeconds;
+    }
+}
